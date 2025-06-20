0fdc729d839de5d2e92b155de37e1b20a282762b
IainHenn <Iainevans2005@gmail.com>
2025-06-20 11:31:30 -0700

    Feature: Resend route

--- backend/src/main/java/com/example/demo/EmailVerificationController.java
+++ backend/src/main/java/com/example/demo/EmailVerificationController.java
@@ -25,9 +25,34 @@ public class EmailVerificationController {
         this.userRepository = userRepository;
         this.emailService = emailService;
     }
-
-    //Need to create a resend route
+    
     //Verify user before they can access dashboard, draw, etc
+
+
+    //Need to integrate this into frontend, limit to 3 resends every day
+    //Need give this option after the initial send, create new box that asks for resend
+    @PostMapping("/resend-token")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<?> resendVerificationToken(@RequestParam String email) {
+        Optional<User> user = userRepository.findByEmail(email);
+        
+        if(user.isPresent()){
+            VerificationToken verificationToken = verificationTokenRepository.findByUser(user.get());
+
+            //If user's token is expired
+            if(java.time.LocalDateTime.now().isAfter(verificationToken.getExpirationDate())){
+                verificationToken.setExpirationDate(java.time.LocalDateTime.now());
+                verificationToken.setToken(java.util.UUID.randomUUID().toString());
+            }
+            
+            emailService.sendEmail(email, "Pictogram: Email Verification", "http://localhost:3000/#/verify?token=" + verificationToken.getToken());
+            
+            return ResponseEntity.ok("Verification token generated and saved.");
+
+        } else {
+            return ResponseEntity.badRequest().body("Invalid or expired token.");
+        }
+    }
     
     @PostMapping("/generate-token")
     @ResponseStatus(HttpStatus.OK)
